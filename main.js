e800780eabba8106a4827a38599d354e3700570f
구름냥 <62387796+gurumnyang@users.noreply.github.com>
2021-07-01 04:25:39 +0900

    hide token

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 const fs = require("fs");
 const client = new Discord.Client();
 
+//토큰 불러오기
 var json1 = fs.readFileSync('token.json', 'utf-8');
 var token = JSON.parse(json1).token;
 
@@ -9,6 +10,7 @@ client.on("ready", () => {
     console.log(`${client.user.tag}에 로그인하였습니다!`);
 });
 client.on("message", msg => {
+    if(msg.author.bot) return;
     if (msg.content === "핑") {
         msg.channel.send("퐁!");
     }
